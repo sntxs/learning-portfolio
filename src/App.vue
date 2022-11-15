04d4642e6a092ebb0ce071db09ac54a265c14d18
sntxs <natanrodrigues649@gmail.com>
2022-11-14 20:55:49 -0400

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+
+<template>
+
+  <body>
+    <header>
+      <nav>
+        <!-- aqui fica a minha "logo" -->
+        <h1 class="logo">sntxs c0d3</h1>
+        <!-- LOGO -->
+
+        <!-- NAV BAR (MENU) -->
+        <ul class="nav-list">
+          <li><a class="opct" href="http://localhost:5173/">Ínicio</a></li>
+          <li><a class="opct" href="http://localhost:5173/">Quem Sou</a></li>
+          <li><a class="opct" href="https://github.com/sntxs">GitHub</a></li>
+          <li><a class="opct" href="http://localhost:5173/">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <div class="bg rounded">
+        <h1 class="text-light">Sejam Bem-Vindos !</h1>
+        <br />
+        <br />
+        <!--
+        <span class="text-light">Este aqui é o meu portifólio.</span> <br />
+        <span class="text-light">É apenas a "primeira versão" dele, espero que gostem ! </span>
+        -->
+        <span class="text-light">
+          Este é o meu primeiro contato com Front-End, então estou brincando com o aprendizado, então logo quis tentar criar um 
+          "portifólio".
+
+          <br/> 
+
+          Aqui está a minha "primeira versão" do meu portifólio, espero que gostem !
+        </span>
+        <br />
+        <br />
+        <br />
+        <br />
+
+
+        <span class="text-light">
+          <strong>
+            /* Click em alguns dos elementos disponíveis na Nav,
+            logo você irá obter informações
+            na qual estão disponibilizadas aqui no portifólio.
+            Obrigado, voltem sempre ! */
+          </strong>
+        </span>
+        <!--
+        <strong>
+          <span class="text-light">/* Click em alguns dos elementos disponíveis na Nav,</span> <br />
+          <span class="text-light">logo você irá obter informações</span> <br />
+          <span class="text-light">na qual estão disponibilizadas aqui no portifólio.</span> <br />
+          <span class="text-light">Obrigado, voltem sempre ! */</span> <br />
+        </strong>
+        -->
+      </div>
+    </main>
+
+  </body>
+
+</template>
